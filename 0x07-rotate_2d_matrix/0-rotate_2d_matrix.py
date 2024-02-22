0b56c1c539e65930b2858b133a046de6eaeca3cb
kabadasori <38851055+kabadasori@users.noreply.github.com>
2024-02-22 15:52:00 +0300

    rotate_2d_matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
-"""2D matrix rotation module.
+"""
+Defines function that can rotate 2D NxN matrix 90 degrees clockwise in place
 """
 
 
 def rotate_2d_matrix(matrix):
-    """Rotates an m by n 2D matrix in place.
     """
-    if type(matrix) != list:
-        return
-    if len(matrix) <= 0:
-        return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
-    rows = len(matrix)
-    cols = len(matrix[0])
-    if not all(map(lambda x: len(x) == cols, matrix)):
-        return
-    c, r = 0, rows - 1
-    for i in range(cols * rows):
-        if i % rows == 0:
-            matrix.append([])
-        if r == -1:
-            r = rows - 1
-            c += 1
-        matrix[-1].append(matrix[r][c])
-        if c == cols - 1 and r >= -1:
-            matrix.pop(r)
-        r -= 1
+    Rotates 2D NxN matrix 90 degrees clockwise in place
+
+    parameters:
+        matrix [list of lists of ints]:
+            matrix to rotate
+            can be assumed to have 2 dimensions and will not be empty
+    """
+    # save original row and column size from input matrix
+    N = len(matrix)
+
+    # build rotated matrix in a separate matrix_copy
+    matrix_copy = []
+    copy_row = 0
+    for column in range(N):
+        for row in range((N - 1), -1, -1):
+            if column is 0:
+                matrix_copy.append([])
+            matrix_copy[copy_row].append(matrix[row][column])
+        copy_row += 1
+
+    # copy contents of matrix_copy into matrix
+    # since matrix is NxN, do not need to resize matrix for rotation
+    for row in range(N):
+        for column in range(N):
+            matrix[row][column] = matrix_copy[row][column]
--- 0x07-rotate_2d_matrix/README.md
+++ 0x07-rotate_2d_matrix/README.md
@@ -1,10 +1,11 @@
-# Rotate 2D Matrix
+# 0x16-rotate_2d_matrix
+This interview algorithm project rotates a 2D NxN matrix in place without importing modules.
 
-This project contains interview coding challenges.
+[Rotate 2D Matrix](/0x16-rotate_2d_matrix/0-rotate_2d_matrix)
+* Write a function in Python `def rotate_2d_matrix(matrix)` that rotates a 2D NxN matrix 90 degrees clockwise:
+  * The Python program cannot import any modules.
+  * The Python program cannot return anything.  The matrix must be edited in-place.
+  * The matrix can be assumed to have two-dimensions and will not be empty.
 
-## Tasks To Complete
-
-+ [x] 0. **Rotate 2D Matrix**<br/>[0-rotate_2d_matrix.py](0-rotate_2d_matrix.py) contains a module with a function that rotates an `n` x `n` 2D matrix 90 degrees clockwise with the following requirements:
-  + Prototype: `def rotate_2d_matrix(matrix):`.
-  + Do not return anything. The matrix must be edited **in-place**.
-  + You can assume the matrix will have 2 dimensions and will not be empty.
+### test_files
+The test_files/ directory contains files used to test the output of the algorithm locally.
